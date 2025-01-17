63a94b347e8013882e1c1acd041abfb4ff05a0f1
pridhi-sabikhi <pridhi1590.becse24@chitkara.edu.in>
2025-01-18 01:35:18 +0530

    Solution to Check Less Than or Equal To

--- Check Less Than or Equal To@@67882a731b4392d8fc1577d9/code.c
+++ Check Less Than or Equal To@@67882a731b4392d8fc1577d9/code.c
@@ -2,7 +2,7 @@
 int main() {
     int num1, num2;
     scanf("%d %d", &num1, &num2);
-    if num1<=num2
+    if (num1<=num2) //yahan () lgani zruri hai python jaise ni krna ke na lgaye
     {
         printf("True");
     }
