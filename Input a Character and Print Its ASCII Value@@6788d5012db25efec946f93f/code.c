af91589a1d402bfa9c3f0b7e08622f9e4e7e3829
pridhi-sabikhi <pridhi1590.becse24@chitkara.edu.in>
2025-01-20 15:52:26 +0530

    Solution to Input a Character and Print Its ASCII Value

--- Input a Character and Print Its ASCII Value@@6788d5012db25efec946f93f/code.c
+++ Input a Character and Print Its ASCII Value@@6788d5012db25efec946f93f/code.c
@@ -2,7 +2,11 @@
 int main(){
     char ch;
     scanf("%c", &ch);
-    //char ko int mein krne see uski acsii value store hoti hai
-    printf("ASCII Value: %d", ch);
+    //char ko int mein krne se uski acsii value print hoti hai
+    // no need to do this int val=(int) ch then print val with %d
+    // sidha print ch with %d bcz compiler sirf %d dekhega and usko kooi frq ni pdta 
+    //ke var kaunsa hai usne bss %d dekha and ch ko %d mein print kr diya 
+    //which is its ascii value
+    printf("ASCII Value: %d", ch); 
     return 0;
 }
