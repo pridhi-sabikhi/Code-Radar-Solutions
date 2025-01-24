f51fffc64c27f1b5b97b95cf0c7cd1ce10ab7f98
pridhi-sabikhi <pridhi1590.becse24@chitkara.edu.in>
2025-01-24 17:10:36 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -5,10 +5,16 @@ int main() {
     char ch;//to i/p operator . here no need of ctype.h bcz koi char func ni lgana hai
     scanf("%d %d %c", &num1, &num2, &ch); //can take %c in separate scanf but no need
     switch(ch){
-        case '+':
-        int ans=num1+num2;
-        printf("%d",ans);
+        case '+':printf("%d",num1+num2);
         break;
+//agr int ans=num1+num2 then printf(ans) krna toh hr case ke baad {}lgao
+        case '-':printf("%d",num1-num2);
+        break;
+        case '*':printf("%d",num1*num2);
+        break;
+        case '/':printf("%d",num1/num2);
+        break;
+        default:printf("error");
     }
     return 0;
 }
