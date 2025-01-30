e83d875e086f382427cc7fb0fd554261fc6fc08a
pridhi-sabikhi <pridhi1590.becse24@chitkara.edu.in>
2025-01-30 13:57:35 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,23 +1,27 @@
 //can do with if
 #include <stdio.h>
 int main() {
-    int num1,num2;
+    int num1,num2, ans;
     char ch;//to i/p operator . here no need of ctype.h bcz koi char func ni lgana hai
     scanf("%d %d", &num1, &num2); // take %c in separate scanf ("%d %d %c", 7num1, &num2, &ch) krne se error aa rha
     scanf("%c",&ch);
     switch(ch){
         case '+':
-        printf("%d",num1+num2);
+        ans=num1+num2;
+        printf("%d",ans);
         break;
-//agr int ans=num1+num2 then printf(ans) krna toh hr case ke baad {}lgao
+//agr int ans=num1+num2 then printf(ans) krna toh bhi hr case ke baad {}ni lgao
         case '-':
-        printf("%d",num1-num2);
+        ans- num1-num2;
+        printf("%d",ans);
         break;
         case '*':
-        printf("%d",num1*num2);
+        ans=num1*num2;
+        printf("%d",ans);
         break;
         case '/':
-        printf("%d",num1/num2);
+        ans=num1/num2;
+        printf("%d",ans);
         break;
         default:
         printf("Error");
