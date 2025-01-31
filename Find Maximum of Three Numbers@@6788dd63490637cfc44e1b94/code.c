40b455b23a11e15b5d65f7a25034a593a3c5ba0e
pridhi-sabikhi <pridhi1590.becse24@chitkara.edu.in>
2025-01-31 16:28:31 +0530

    Solution to Find Maximum of Three Numbers

--- Find Maximum of Three Numbers@@6788dd63490637cfc44e1b94/code.c
+++ Find Maximum of Three Numbers@@6788dd63490637cfc44e1b94/code.c
@@ -13,11 +13,32 @@
 // }
 
 //using ternary
+// #include <stdio.h>
+// int main(){
+//     int a, b,c;
+//     scanf("%d %d %d", &a, &b, &c);
+//     (a>b && a>c)? printf("%d", a):(b>c && b>a)? printf("%d", b):printf("%d", c);
+//     return 0;
+// }
+
+//using switch
 #include <stdio.h>
 int main(){
-    int a, b,c;
+    int a,b,c;
     scanf("%d %d %d", &a, &b, &c);
-    (a>b && a>c)? printf("%d", a):(b>c && b>a)? printf("%d", b):printf("%d", c);
-    return 0;
-
+    switch(a>b && a>c){
+        case 1:
+        printf("%d", a);
+        break;
+        case 0:
+        switch(b>c && b>a){
+            case 1:
+            printf("%d", b);
+            break;
+            case 0:
+            printf("%d", c);
+            break;
+        }
+        break;// vaise can skip this or isse just pehle wali dono mein se koi 1 bbut dono hi nhi skip krni
+    }
 }
