b36677bcf6859751b34925a06e2ce0b145b1cc92
pridhi-sabikhi <pridhi1590.becse24@chitkara.edu.in>
2025-02-01 20:00:04 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -1,39 +1,40 @@
-// #include <stdio.h>
-// #include <ctype.h>
-// int main() {
-//     char ch;
-//     scanf("%c", &ch);
-//     if(isalpha(ch)){
-//         ch= tolower(ch);
-//         if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
-//         printf("Vowel");
-//         else
-//         printf("Consonant");
-//     }
-//     else{
-//         if (isdigit(ch))
-//         printf("Digit");
-//         else
-//         printf("Special Character");
-//     }
-//     return 0;
-// }
-
-//cp method not convert to lower case and check digit using > <
+//do this 
 #include <stdio.h>
-int main(){
+#include <ctype.h>
+int main() {
     char ch;
     scanf("%c", &ch);
-    if((ch>='A' && ch<='Z') || (ch>='a' && ch<='z'))
-    {
-    if (ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U' || ch=='a' || ch=='e' || ch=='i'|| ch=='o' || ch=='u')
-    printf("Vowel");
-    else
-    printf("Consonant");
+    if(isalpha(ch)){
+        ch= tolower(ch);
+        if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+        printf("Vowel");
+        else
+        printf("Consonant");
     }
-    else if(ch>='0' && ch<='9')
-    printf("Digit");
-    else 
-    printf("Special Character");
+    else{
+        if (isdigit(ch))
+        printf("Digit");
+        else
+        printf("Special Character");
+    }
     return 0;
 }
+
+//cp method not convert to lower case and check digit using > <
+// #include <stdio.h>
+// int main(){
+//     char ch;
+//     scanf("%c", &ch);
+//     if((ch>='A' && ch<='Z') || (ch>='a' && ch<='z'))
+//     {
+//     if (ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U' || ch=='a' || ch=='e' || ch=='i'|| ch=='o' || ch=='u')
+//     printf("Vowel");
+//     else
+//     printf("Consonant");
+//     }
+//     else if(ch>='0' && ch<='9')
+//     printf("Digit");
+//     else 
+//     printf("Special Character");
+//     return 0;
+// }
