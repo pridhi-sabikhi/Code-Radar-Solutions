2ec1fd039d56a941675c8336cf86f14bc6cb33b2
pridhi-sabikhi <pridhi1590.becse24@chitkara.edu.in>
2025-02-02 14:35:18 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,31 +1,12 @@
-//can do with if
-#include <stdio.h>
-int main() {
-    int num1,num2, ans;
-    char ch;//to i/p operator . here no need of ctype.h bcz koi char func ni lgana hai
-    scanf("%d %d", &num1, &num2); // chahe take %c in separate scanf or 1 put \n after %d vrana ch takes space as i/p 
-         //same line mein lena toh do like this
-         scanf(" %c", &ch);
-    switch(ch){
-        case '+':
-        ans=num1+num2;
-        printf("%d",ans);
-        break;
-//agr int ans=num1+num2 then printf(ans) krna toh bhi hr case ke baad {}ni lgao
-        case '-':
-        ans= num1-num2;
-        printf("%d", ans);
-        break;
-        case '*':
-        ans=num1*num2;
-        printf("%d", ans);
-        break;
-        case '/':
-        ans=num1/num2;
-        printf("%d", ans);
-        break;
-        default:
-        printf("Error");
-       }
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long a,b; cin >> a >> b;
+    char ch; cin >> ch;
+    if(ch == '+') cout << a + b << endl;
+    if(ch == '-') cout << a - b << endl;
+    if(ch == '*') cout << a*b << endl;
+    else cout << a/b << endl;
     return 0;
+
 }
