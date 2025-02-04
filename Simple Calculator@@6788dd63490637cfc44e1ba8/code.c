d2cbe64c18deab333edec2b693c6da7a6c50286d
pridhi-sabikhi <pridhi1590.becse24@chitkara.edu.in>
2025-02-04 17:17:36 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,12 +1,27 @@
-#include<iostream>
-using namespace std;
+#include <stdio.h>
 int main(){
-    long long a,b; cin >> a >> b;
-    char ch; cin >> ch;
-    if(ch == '+') cout << a + b << endl;
-    if(ch == '-') cout << a - b << endl;
-    if(ch == '*') cout << a*b << endl;
-    else cout << a/b << endl;
+    int num1, num2, ans;
+    char ch;
+    scanf("%d %d %c", &num1, &num2 ,&ch);
+    switch(ch){
+        case '+':
+        ans=num1+num2;
+        printf("%d",ans);
+        break;
+        case '-':
+        ans=num1-num2;
+        printf("%d", ans);
+        break;
+        case '*':
+        ans=num1*num2;
+        printf("%d", ans);
+        break;
+        case '/':
+        ans=num1/num2;
+        printf("%d", ans);
+        break;
+        default:
+        printf("Error");
+    }
     return 0;
-
 }
