abcc4beb630fed28e34efc52441678971db2ab18
pridhi-sabikhi <pridhi1590.becse24@chitkara.edu.in>
2025-02-06 12:36:35 +0530

    Solution to Print a Pyramid Pattern

--- Print a Pyramid Pattern@@67877e1818d53eb724f435bf/code.c
+++ Print a Pyramid Pattern@@67877e1818d53eb724f435bf/code.c
@@ -4,9 +4,14 @@ int main() {
     scanf("%d", &n);
     for(int row=0; row<n; row++)
     {
+        for(int col=0; col<n-row-1; col++)
+        {
+            printf(" ");
+        }
         for(int col=0; col<2*row+1; col++)
         {
-            printf("*");
+            printf("*");// here no space after * bcz pehle for loop ses space print ki so ab agr space lgai toh error
+            //jis Q mein spaces print krne ke liye alg se for loop lge usmein not put space with * vrna put
         }
         printf("\n");
     }
