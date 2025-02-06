539f06ea0b6ee8f0e97386b8f60d95cd55e33153
pridhi-sabikhi <pridhi1590.becse24@chitkara.edu.in>
2025-02-06 12:09:40 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -2,9 +2,8 @@
 int main(){
     int num1, num2, add, sub,mul,div;
     char ch;
-   
-    scanf("%d %d %c", &num1, &num2, &ch);
-     //scanf("%c", &ch);
+    scanf("%d %d", &num1, &num2);
+    scanf("%c", &ch);
     switch(ch)
     {
         case '+':
@@ -22,7 +21,13 @@ int main(){
         case '/':
         
         div=num1/num2;
+        if(num2==0)
+        {
+            printf("error");
+        }
+        else{
         printf("%d", div);
+        }
         break;
         default:
         printf("error");
