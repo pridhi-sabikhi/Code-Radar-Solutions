72996106125d40dab3776ae26ba9bbee5250115d
pridhi-sabikhi <pridhi1590.becse24@chitkara.edu.in>
2025-02-10 17:26:59 +0530

    Solution to Convert Decimal to Binary

--- Convert Decimal to Binary@@67882a731b4392d8fc1577f5/code.c
+++ Convert Decimal to Binary@@67882a731b4392d8fc1577f5/code.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
+
 int main() {
-    int num, s=0, r=0;
+    int num, s = 0, r = 0;
+    int place = 1; // Variable to track the place value for binary digits (1, 10, 100, ...)
     scanf("%d", &num);
-    while(num>0)
-    {
-        r=num%2;
-        s=r+(s*10);//r=1 then s=1 then r=1 then s= 1+10x+1=11 
-        num=num/2;
+
+    while (num > 0) {
+        r = num % 2;                  // Get the remainder (0 or 1)
+        s = s + (r * place);          // Add the binary digit to the correct place value
+        num = num / 2;                // Divide the number by 2
+        place = place * 10;           // Shift the place value left (1, 10, 100, ...)
     }
-    printf("%d", s);
+
+    // Print the binary number
+    printf("%d\n", s);
+
     return 0;
 }
+
 //did ulta bcz we print niche se uppar
