ed69650068888074b05c9bc3de74cb2dc51c04a6
pridhi-sabikhi <pridhi1590.becse24@chitkara.edu.in>
2025-02-12 19:49:50 +0530

    Solution to Count Vowels in a String

--- Count Vowels in a String@@676c039d80e1714600f67641/code.c
+++ Count Vowels in a String@@676c039d80e1714600f67641/code.c
@@ -10,7 +10,8 @@ int main()
     int l=strlen(str);
     for(int i=0; i<l; i++) //<l bcz index is from 0 to l-1
     {
-        if (str[i]==isalpha(str[i])) //bcz string can contain alphanumeric  but his is for char only so write ctype.h also
+        if (isalpha(str[i])) //bcz string can contain alphanumeric  but his is for char only so write ctype.h also
+        //not written str[i]==isaplhastr[i] bc isalpha return boolean val but we are comparing it with char so its wrong
         {
             str[i]=tolower(str[i]); //to convert each char to lower taaki up case vowels na check pre
             if(str[i]=='a'|| str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u')
