136cbc9236a3f6c8d20a490551e75492fb1d78cb
pridhi-sabikhi <pridhi1590.becse24@chitkara.edu.in>
2025-02-12 21:29:32 +0530

    Solution to Perform Bitwise One's Complement

--- Perform Bitwise One's Complement@@67882a731b4392d8fc1577e7/code.c
+++ Perform Bitwise One's Complement@@67882a731b4392d8fc1577e7/code.c
@@ -2,7 +2,7 @@
 int main(){
     int num;
     scanf("%d", &num);
-    int ans=~num;//can write ~(num)
+    int ans=(~(num));//can write ~(num) or (~(num))
     //not correct !(num)  or !num
     printf("%d", ans);
     return 0;
