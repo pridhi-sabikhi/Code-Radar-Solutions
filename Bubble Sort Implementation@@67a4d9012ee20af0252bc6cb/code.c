cee78239fcfa7eb0092d9033a4922ecb43d41ca0
pridhi-sabikhi <sabhikhimonika@gmail.com>
2025-02-24 14:31:18 +0530

    Solution to Bubble Sort Implementation

---
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -0,0 +1,22 @@
+int bubbleSort(int arr, int n)
+{
+    int l=arr.size();
+    for(int i=0; i<n; i++)
+    {
+        if(arr[i]>arr[i+1])
+        {
+            int temp=arr[i];
+            arr[i]=arr[i+1];
+            arr[i+1]=temp;
+        }
+    }
+    return arr;
+}
+void printArray(int arr, int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
