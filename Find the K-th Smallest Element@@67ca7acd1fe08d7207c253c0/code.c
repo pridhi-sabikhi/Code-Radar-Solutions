b54039ea9f8df81f802db596f19d78d189d185d9
pridhi-sabikhi <sabhikhimonika@gmail.com>
2025-03-11 15:37:07 +0530

    Solution to Find the K-th Smallest Element

--- Find the K-th Smallest Element@@67ca7acd1fe08d7207c253c0/code.c
+++ Find the K-th Smallest Element@@67ca7acd1fe08d7207c253c0/code.c
@@ -1,23 +1,29 @@
-//k times minimum wala loop chlao
-#include <limits.h>
-int mini(int arr[], int n)
-{
-    int brr[n]=arr[n];
-    int min=INT_MAX;
-    for(int i=0; i<n; i++)
-    {
-        if(min<brr[i])
-        {
-            min=brr[i];
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        // Swap the found minimum element with the first element
+        if (minIndex != i) {
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
         }
     }
-    return min;
 }
-int kthsmallest(int arr[], int n, int k)
-{
-    for(int i=1; i<=k; i++)
-    {
-        int ans=mini(arr,n);        
+
+int findKthSmallest(int arr[], int n, int k) {
+    // Check if k is out of range
+    if (k < 1 || k > n) {
+        return -1;  // k is out of range
     }
-     printf("%d", ans);
+
+    // Sort the array using selection sort
+    selectionSort(arr, n);
+
+    // Return the k-th smallest element (k-1 for zero-based index)
+    return arr[k - 1];
 }
