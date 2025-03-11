87f9fe484a5852d0c4f534caa57a42667909c18d
pridhi-sabikhi <sabhikhimonika@gmail.com>
2025-03-11 15:05:13 +0530

    Solution to Kth Missing Positive Number

--- Kth Missing Positive Number@@67ca7acd1fe08d7207c253c5/code.c
+++ Kth Missing Positive Number@@67ca7acd1fe08d7207c253c5/code.c
@@ -1,24 +1,39 @@
-#include <stdio.h>
+// int findKthMissing(int arr[], int n, int k) {
+//     int current = 1;  // Start checking from the first positive integer
+//     int missingCount = 0;  // Count of missing positive integers
+//     int index = 0;  // Index for the array
 
-int findKthMissing(int arr[], int n, int k) {
-    int current = 1;  // Start checking from the first positive integer
-    int missingCount = 0;  // Count of missing positive integers
-    int index = 0;  // Index for the array
+//     while (missingCount < k) {
+//         if (index < n && arr[index] == current) {
+//             // If the current number is in the array, move to the next number
+//             index++;
+//         } else {
+//             // If the current number is missing, increment the missing count
+//             missingCount++;
+//             if (missingCount == k) {
+//                 return current;  // Return the k-th missing positive integer
+//             }
+//         }
+//         current++;  // Move to the next positive integer
+//     }
 
-    while (missingCount < k) {
-        if (index < n && arr[index] == current) {
-            // If the current number is in the array, move to the next number
-            index++;
-        } else {
-            // If the current number is missing, increment the missing count
-            missingCount++;
-            if (missingCount == k) {
-                return current;  // Return the k-th missing positive integer
-            }
+//     return -1;  // In case k is out of bounds (not expected in this problem)
+// }
+
+int findKthMissing(int arr[], int n, int k)
+{
+    int start=1, c=0;
+    for(int i=0; i<n; i++)
+    {
+        if(start == arr[i])
+        {
+            start++;
         }
-        current++;  // Move to the next positive integer
+        else{
+            c++;
+            if(c==k)
+            return start;
+        }
+
     }
-
-    return -1;  // In case k is out of bounds (not expected in this problem)
 }
-
