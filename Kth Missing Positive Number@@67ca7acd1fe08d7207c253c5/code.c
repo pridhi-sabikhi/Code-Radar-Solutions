40bb478033f8e0e22d5ac4ac538524966a7e693a
pridhi-sabikhi <sabhikhimonika@gmail.com>
2025-03-11 15:01:55 +0530

    Solution to Kth Missing Positive Number

---
+++ Kth Missing Positive Number@@67ca7acd1fe08d7207c253c5/code.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int findKthMissing(int arr[], int n, int k) {
+    int current = 1;  // Start checking from the first positive integer
+    int missingCount = 0;  // Count of missing positive integers
+    int index = 0;  // Index for the array
+
+    while (missingCount < k) {
+        if (index < n && arr[index] == current) {
+            // If the current number is in the array, move to the next number
+            index++;
+        } else {
+            // If the current number is missing, increment the missing count
+            missingCount++;
+            if (missingCount == k) {
+                return current;  // Return the k-th missing positive integer
+            }
+        }
+        current++;  // Move to the next positive integer
+    }
+
+    return -1;  // In case k is out of bounds (not expected in this problem)
+}
+
