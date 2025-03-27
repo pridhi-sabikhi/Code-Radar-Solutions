4b5281584793a1dbdb8744e672398740214d1fc7
pridhi-sabikhi <sabhikhimonika@gmail.com>
2025-03-27 16:35:23 +0530

    Solution to Selection Sort for Integers

---
+++ Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+// Function to perform selection sort on an array of integers
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        // Assume the minimum is the first element of the unsorted part
+        int minIndex = i;
+        
+        // Find the index of the minimum element in the unsorted part
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        
+        // Swap the found minimum element with the first element of the unsorted part
+        if (minIndex != i) {
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+}
+
+// Function to print the array of integers
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        if (i > 0) {
+            printf(" "); // Print space before the next number
+        }
+        printf("%d", arr[i]);
+    }
+    printf("\n"); // Print newline at the end
+}
