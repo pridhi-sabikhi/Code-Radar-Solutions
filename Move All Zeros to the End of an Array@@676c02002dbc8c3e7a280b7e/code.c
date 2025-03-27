d3651510ca89730b94652d78be7a3851e364dde2
pridhi-sabikhi <sabhikhimonika@gmail.com>
2025-03-27 23:01:16 +0530

    Solution to Move All Zeros to the End of an Array

--- Move All Zeros to the End of an Array@@676c02002dbc8c3e7a280b7e/code.c
+++ Move All Zeros to the End of an Array@@676c02002dbc8c3e7a280b7e/code.c
@@ -2,35 +2,36 @@
 int new_array(int arr[], int n)
 {
     //if want to move all 0s to starting and koi order no. ni change krna 
-    int count=n-1;
-    for(int i=n-1; i>0; i--)//start checking from n-1 bcz reverse mein order ni change krna
+//     int count=n-1;
+//     for(int i=n-1; i>0; i--)//start checking from n-1 bcz reverse mein order ni change krna
+//     {
+//         if(arr[i]!=0)
+//         {
+//             arr[count]=arr[i];
+//             count--;
+//         }
+//     }
+//     while(count>=0)//ab 0 index tkk sbhi mein 0 daal do 
+//     {
+//         arr[count]=0;
+//         count--;
+//     }
+// }
+
+    int count=0;
+    for(int i=0; i<n; i++)
     {
-        if(arr[i]!=0)
+        if (arr[i]!=0)
         {
-            arr[count]=arr[i];
-            count--;
+            arr[count++]=arr[i];// i.e startint pr vo element daaldo
         }
     }
-    while(count>=0)//ab 0 index tkk sbhi mein 0 daal do 
+    //now  fill remaining indexes with 0s
+    while(count<n)
     {
-        arr[count]=0;
-        count--;
+        arr[count++]=0;
     }
 }
-
-    // int count=0;
-    // for(int i=0; i<n; i++)
-    // {
-    //     if (arr[i]!=0)
-    //     {
-    //         arr[count++]=arr[i];// i.e startint pr vo element daaldo
-    //     }
-    // }
-    // //now  fill remaining indexes with 0s
-    // while(count<n)
-    // {
-    //     arr[count++]=0;
-    // }
 
 int print(int arr[], int n)
 {
