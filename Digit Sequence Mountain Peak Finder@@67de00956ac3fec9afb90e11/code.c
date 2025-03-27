ffadff3cb2edae404f069bb417727d4bb9b7c9c0
pridhi-sabikhi <sabhikhimonika@gmail.com>
2025-03-27 15:56:30 +0530

    Solution to Digit Sequence Mountain Peak Finder

--- Digit Sequence Mountain Peak Finder@@67de00956ac3fec9afb90e11/code.c
+++ Digit Sequence Mountain Peak Finder@@67de00956ac3fec9afb90e11/code.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <string.h>
 int mountainPeak(int N) {
     char str[12]; // Enough to hold up to 10 digits + null terminator
     sprintf(str, "%d", N); // Convert integer to string
