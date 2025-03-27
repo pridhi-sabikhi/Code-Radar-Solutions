69f775835226e74b134174acc6608c2f53433350
pridhi-sabikhi <sabhikhimonika@gmail.com>
2025-03-27 21:36:30 +0530

    Solution to Rotate Array Elements

--- Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -17,38 +17,22 @@ int main() {
     k = k % n; // If k is greater than n, we only need to rotate k % n times
 
     // Create a temporary array to hold the rotated values
-    int temp=0;
-            temp=arr[n-1];
-            arr[n-1]=arr[1];
-            arr[1]=temp;
-        
-            temp=arr[n-2];
-            arr[n-2]=arr[0];
-            arr[0]=temp;
-        
-       for(int i=2; i<n; i++)
-    
-        {
-            temp=arr[i];
-            arr[i]=arr[i+k];
-            arr[i+k]=arr[i];
-        }
-    
+    int temp[n];
 
-    // // Copy the last k elements to the beginning of the temp array
-    // for (int i = 0; i < k; i++) {
-    //     temp[i] = arr[n - k + i];
-    // }
+    // Copy the last k elements to the beginning of the temp array
+    for (int i = 0; i < k; i++) {
+        temp[i] = arr[n - k + i];
+    }
 
-    // // Copy the remaining elements to the temp array
-    // for (int i = 0; i < n - k; i++) {
-    //     temp[k + i] = arr[i];
-    // }
+    // Copy the remaining elements to the temp array
+    for (int i = 0; i < n - k; i++) {
+        temp[k + i] = arr[i];
+    }
 
-    // // Copy the temp array back to the original array
-    // for (int i = 0; i < n; i++) {
-    //     arr[i] = temp[i];
-    // }
+    // Copy the temp array back to the original array
+    for (int i = 0; i < n; i++) {
+        arr[i] = temp[i];
+    }
 
     // Print the rotated array
     for (int i = 0; i < n; i++) {
