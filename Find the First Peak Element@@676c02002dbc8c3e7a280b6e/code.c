26072681d20c78ff522c37a162eff489c1cb0be1
pridhi-sabikhi <sabhikhimonika@gmail.com>
2025-03-27 22:08:24 +0530

    Solution to Find the First Peak Element

--- Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
+++ Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
@@ -10,37 +10,32 @@ int main()
     for(int i=0; i<n; i++)//start from 0 vrna  error aa rha hai
     {
         scanf("%d", &arr[i]);
-    }
-    int peak = -1;
-
-    // Check for the first element
-    if (n == 1) {
-        peak = arr[0]; // Only one element is a peak
-    } 
-    else if (arr[0] > arr[1]) 
-    {
-        peak = arr[0]; // First element is a peak
-    } 
-    else if
-    {
-        for(int i=1; i<n-1; i++)//start from 1 bcz 0 kiya toh can't check i-1, n-1 bcz for last index  i+1 index hoga hi nhi
+    }  
+    for(int i=1; i<n-1; i++)//start from 1 bcz 0 kiya toh can't check i-1, n-1 bcz for last index  i+1 index hoga hi nhi
     {
         //checking ke mid mein toh nhi greatest
         if(arr[i]>arr[i+1] && arr[i]>arr[i-1])
         {
-        peak= arr[i];
+        printf("%d", arr[i]);
+        return 0;//exit prog don't execute anything
         }
     }
-    }
-    else if(arr[n-1]>arr[n-2])
+        //now check 1st and last bcz vo no. skip kiye thee for loop mein
+        //if nhi then check ke 1st no. toh nhi greatest
+        if(arr[0]>arr[1])
         {
-        peak= arr[n-1];
+        printf("%d", arr[0]);
+        
+        }
+        //agr yeh bhi nhi toh check ke last no. is gretatest
+        if(arr[n-1]>arr[n-2])
+        {
+        printf("%d", arr[n-1]);
         
         }
         //w.m no peak no.
         else{
-        peak=-1; //if no peak element
+        printf("-1"); //if no peak element
         }
-        printf("%d", peak);
     return 0;
 }
