ea8ec22a90a17009a03a9e7632eb3120580e508d
pridhi-sabikhi <sabhikhimonika@gmail.com>
2025-03-27 14:59:41 +0530

    Solution to Find the Duplicate Element in an Array

--- Find the Duplicate Element in an Array@@676c02002dbc8c3e7a280b7b/code.c
+++ Find the Duplicate Element in an Array@@676c02002dbc8c3e7a280b7b/code.c
@@ -1,32 +1,31 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main() {
     int n;
     scanf("%d", &n);
     
     int arr[n];
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
     // Start checking for duplicates
-    int i = 0;
-    while (i < n) {
-        int index = arr[i]; // Use arr[i] as the index
+    for (int i = 0; i < n; i++) {
+        // Use the absolute value to find the index
+        int index = abs(arr[i]);
 
-        // Make sure index is within valid bounds
-        if (index < n && arr[i] != arr[index]) {
-            // Swap if values are not equal
-            int temp = arr[i];
-            arr[i] = arr[index];
-            arr[index] = temp;
+        // Check if the value at this index is negative
+        if (arr[index] < 0) {
+            // If it's negative, we found the duplicate
+            printf("%d\n", index);
+            return 0; // Exit after finding the duplicate
         } else {
-            // If duplicate is found or already visited, move to next element
-            i++;
+            // Mark the value at this index as negative
+            arr[index] = -arr[index];
         }
     }
 
-    // After the loop, arr[0] should hold the duplicate value
-    printf("%d", arr[0]);
+    // If no duplicate is found
+    printf("-1\n");
     return 0;
 }
