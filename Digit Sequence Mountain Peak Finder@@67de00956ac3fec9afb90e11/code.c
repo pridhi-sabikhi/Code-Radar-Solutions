cac63cd8d645215a0a24c38707deb80752899a99
pridhi-sabikhi <sabhikhimonika@gmail.com>
2025-03-27 16:10:31 +0530

    Solution to Digit Sequence Mountain Peak Finder

--- Digit Sequence Mountain Peak Finder@@67de00956ac3fec9afb90e11/code.c
+++ Digit Sequence Mountain Peak Finder@@67de00956ac3fec9afb90e11/code.c
@@ -1,62 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 
-// int mountainPeak(int N) {
-//     char str[12]; // Enough to hold up to 10 digits + null terminator
-//     sprintf(str, "%d", N); // Convert integer to string
-
-//     int length = strlen(str);
-    
-//     // Check if the number has at least 3 digits
-//     if (length < 3 || length%2==0) {
-//         return -1;
-//     }
- 
-//     int i = 0;
-
-//     Ascending part
-//     while (i < length - 1 && str[i] < str[i + 1]) {
-//         i++;
-//     }
-
-//     // Check if we have a peak
-//     if (i == 0 || i == length - 1) {
-//         return -1; // No peak or peak at the ends
-//     }
-
-//     // Descending part
-//     while (i < length - 1 && str[i] > str[i + 1]) {
-//         i++;
-//     }
-
-//     // If we reached the end, we have a valid mountain
-//     if (i == length - 1) {
-//         return str[i - 1] - '0'; // Return the peak digit as an integer
-//     }
-
 int mountainPeak(int N) {
-    // Convert the number to an array of digits
-    int digits[10]; // Assuming N has at most 10 digits
-    int length = 0;
+    char str[12]; // Enough to hold up to 10 digits + null terminator
+    sprintf(str, "%d", N); // Convert integer to string
 
-    // Extract digits
-    while (N > 0) {
-        digits[length++] = N % 10; // Get the last digit
-        N /= 10; // Remove the last digit
+    int length = strlen(str);
+    
+    // Check if the number has at least 3 digits
+    if (length < 3 || length%2==0) {
+        return -1;
     }
-
-    // Reverse the digits to get the correct order
-    for (int i = 0; i < length / 2; i++) {
-        int temp = digits[i];
-        digits[i] = digits[length - 1 - i];
-        digits[length - 1 - i] = temp;
-    }
-
-    // Check for mountain pattern
+ 
     int i = 0;
 
-    // Ascending part
-    while (i < length - 1 && digits[i] < digits[i + 1]) {
+//     Ascending part
+    while (i < length - 1 && str[i] < str[i + 1]) {
         i++;
     }
 
@@ -66,17 +25,11 @@ int mountainPeak(int N) {
     }
 
     // Descending part
-    while (i < length - 1 && digits[i] > digits[i + 1]) {
+    while (i < length - 1 && str[i] > str[i + 1]) {
         i++;
     }
 
     // If we reached the end, we have a valid mountain
     if (i == length - 1) {
-        return digits[i - 1]; // Return the peak digit
+        return str[i] - '0'; // Return the peak digit as an integer
     }
-
-    return -1; // Not a mountain
-}
-
-
-   
