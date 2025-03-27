8dd4debd2369f0c8248d58b200f78c60bdf834cc
pridhi-sabikhi <sabhikhimonika@gmail.com>
2025-03-27 16:04:44 +0530

    Solution to Digit Sequence Mountain Peak Finder

--- Digit Sequence Mountain Peak Finder@@67de00956ac3fec9afb90e11/code.c
+++ Digit Sequence Mountain Peak Finder@@67de00956ac3fec9afb90e11/code.c
@@ -8,31 +8,37 @@ int mountainPeak(int N) {
     int length = strlen(str);
     
     // Check if the number has at least 3 digits
-    if (length < 3) {
+    if (length < 3 || length%2==0) {
         return -1;
     }
+    for(int i=0; i<length/2; i++)
+    {
+        if(str[i]==str[length-i-1])
+        {
 
-    int i = 0;
+        }
+    }
+    //int i = 0;
 
     // Ascending part
-    while (i < length - 1 && str[i] < str[i + 1]) {
-        i++;
-    }
+    // while (i < length - 1 && str[i] < str[i + 1]) {
+    //     i++;
+    // }
 
-    // Check if we have a peak
-    if (i == 0 || i == length - 1) {
-        return -1; // No peak or peak at the ends
-    }
+    // // Check if we have a peak
+    // if (i == 0 || i == length - 1) {
+    //     return -1; // No peak or peak at the ends
+    // }
 
-    // Descending part
-    while (i < length - 1 && str[i] > str[i + 1]) {
-        i++;
-    }
+    // // Descending part
+    // while (i < length - 1 && str[i] > str[i + 1]) {
+    //     i++;
+    // }
 
-    // If we reached the end, we have a valid mountain
-    if (i == length - 1) {
-        return str[i - 1] - '0'; // Return the peak digit as an integer
-    }
+    // // If we reached the end, we have a valid mountain
+    // if (i == length - 1) {
+    //     return str[i - 1] - '0'; // Return the peak digit as an integer
+    // }
 
     return -1; // Not a mountain
 }
