3d0ad70a7b122b780b6997888a4c7ceead17243c
pridhi-sabikhi <sabhikhimonika@gmail.com>
2025-03-27 22:58:02 +0530

    Solution to Move All Zeros to the End of an Array

--- Move All Zeros to the End of an Array@@676c02002dbc8c3e7a280b7e/code.c
+++ Move All Zeros to the End of an Array@@676c02002dbc8c3e7a280b7e/code.c
@@ -1,19 +1,36 @@
 #include<stdio.h>
 int new_array(int arr[], int n)
 {
-    int count=0;
-    for(int i=0; i<n; i++)
+    //if want to move all 0s to starting and koi order no. ni change krna 
+    int count=n-1;
+    for(int i=n-1; i>0; i++)//start checking from n-1 bcz reverse mein order ni change krna
     {
-        if (arr[i]!=0)
+        if(arr[i]!=0)
         {
-            arr[count++]=arr[i];// i.e startint pr vo element daaldo
+            arr[count]=arr[i];
+            count--;
         }
     }
-    //now  fill remaining indexes with 0s
-    while(count<n)
+    while(count>=0)//ab 0 index tkk sbhi mein 0 daal do 
     {
-        arr[count++]=0;
+        arr[count]=0;
+        count--;
+
     }
+
+    // int count=0;
+    // for(int i=0; i<n; i++)
+    // {
+    //     if (arr[i]!=0)
+    //     {
+    //         arr[count++]=arr[i];// i.e startint pr vo element daaldo
+    //     }
+    // }
+    // //now  fill remaining indexes with 0s
+    // while(count<n)
+    // {
+    //     arr[count++]=0;
+    // }
 }
 int print(int arr[], int n)
 {
