1c0070b3307280f8e327d1aa87fd160d61375caf
pridhi-sabikhi <sabhikhimonika@gmail.com>
2025-03-27 22:12:35 +0530

    Solution to Find the First Peak Element

--- Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
+++ Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
@@ -11,6 +11,18 @@ int main()
     {
         scanf("%d", &arr[i]);
     }  
+    if(n==1)
+    {
+        printf("%d", arr[0]);//vohi peak hai 
+        return 0;
+    }
+
+    if(arr[0]>arr[1])
+    {
+        printf("%d", arr[0])
+        return 0;
+    }
+    //if nhi then check mid mein
     for(int i=1; i<n-1; i++)//start from 1 bcz 0 kiya toh can't check i-1, n-1 bcz for last index  i+1 index hoga hi nhi
     {
         //checking ke mid mein toh nhi greatest
@@ -20,18 +32,11 @@ int main()
         return 0;//exit prog don't execute anything
         }
     }
-        //now check 1st and last bcz vo no. skip kiye thee for loop mein
-        //if nhi then check ke 1st no. toh nhi greatest
-        if(arr[0]>arr[1])
-        {
-        printf("%d", arr[0]);
-        
-        }
         //agr yeh bhi nhi toh check ke last no. is gretatest
         if(arr[n-1]>arr[n-2])
         {
         printf("%d", arr[n-1]);
-        
+        //here no need to return
         }
         //w.m no peak no.
         else{
