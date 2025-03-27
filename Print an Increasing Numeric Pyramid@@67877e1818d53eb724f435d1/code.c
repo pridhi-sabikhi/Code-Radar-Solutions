68928b2a7640c2fd5ee0576c72fb1ce7d548bd07
pridhi-sabikhi <sabhikhimonika@gmail.com>
2025-03-27 19:13:18 +0530

    Solution to Print an Increasing Numeric Pyramid

--- Print an Increasing Numeric Pyramid@@67877e1818d53eb724f435d1/code.c
+++ Print an Increasing Numeric Pyramid@@67877e1818d53eb724f435d1/code.c
@@ -4,14 +4,21 @@ int main() {
     scanf("%d", &N);
     for(int row=0; row<N; row++)
     {
+        int start=1;
         for(int col=0; col<N-row-1; col++)
         {
             printf(" ");
         }
-        for(int col=0;col<row+1;col++)
+        for(int col=0; col<row+1; col++)
         {
-            printf("%d ", col+1);//we want to print 1 at col 0 with space 
+            printf("%d ", start);
+            start++;
         }
+        //can do like this but for same alpha pattern start use kiy aso here also use strat 
+        // for(int col=0;col<row+1;col++)
+        // {
+        //     printf("%d ", col+1);//we want to print 1 at col 0 with space 
+        // }
         printf("\n");
     }
     return 0;
