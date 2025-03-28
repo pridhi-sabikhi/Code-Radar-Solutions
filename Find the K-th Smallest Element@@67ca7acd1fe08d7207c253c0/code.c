8fd9c18431b023ca986a1f3521e20be8cc776ca2
pridhi-sabikhi <sabhikhimonika@gmail.com>
2025-03-29 02:04:33 +0530

    Solution to Find the K-th Smallest Element

--- Find the K-th Smallest Element@@67ca7acd1fe08d7207c253c0/code.c
+++ Find the K-th Smallest Element@@67ca7acd1fe08d7207c253c0/code.c
@@ -1,18 +1,26 @@
-void selectionSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        int minIndex = i;
-        for (int j = i + 1; j < n; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
-            }
-        }
-        // Swap the found minimum element with the first element
-        if (minIndex != i) {
-            int temp = arr[i];
-            arr[i] = arr[minIndex];
-            arr[minIndex] = temp;
-        }
-    }
+// void selectionSort(int arr[], int n) {
+//     for (int i = 0; i < n - 1; i++) {
+//         int minIndex = i;
+//         for (int j = i + 1; j < n; j++) {
+//             if (arr[j] < arr[minIndex]) {
+//                 minIndex = j;
+//             }
+//         }
+//         // Swap the found minimum element with the first element
+//         if (minIndex != i) {
+//             int temp = arr[i];
+//             arr[i] = arr[minIndex];
+//             arr[minIndex] = temp;
+//         }
+//     }
+// }
+int compare(const void *a, const void *b)
+{
+    return (*(int *)a - *(int *)b);
+}
+int selectionSort(int arr[], int n, int k)
+{
+    qsort(arr, n, sizeof(int), compare);
 }
 
 int kthSmallest(int arr[], int n, int k) {
