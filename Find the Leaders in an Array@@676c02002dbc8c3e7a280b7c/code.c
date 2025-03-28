7df2a29468e6ba058ce0505aefc7ff6dabb364e0
pridhi-sabikhi <sabhikhimonika@gmail.com>
2025-03-28 10:28:06 +0530

    Solution to Find the Leaders in an Array

--- Find the Leaders in an Array@@676c02002dbc8c3e7a280b7c/code.c
+++ Find the Leaders in an Array@@676c02002dbc8c3e7a280b7c/code.c
@@ -8,37 +8,51 @@ int main() {
         return 0; // No elements to process
     }
 
-    int nums[100]; // Assuming the maximum size of the array is 100
+    int arr[N]; // Assuming the maximum size of the array is 100
 
     // Read the array elements
     for (int i = 0; i < N; i++) {
-        scanf("%d", &nums[i]);
+        scanf("%d", &arr[i]);
     }
-
-    // Array to store leaders
-    int leaders[100]; // Assuming the maximum number of leaders is 100
-    int leaderCount = 0;
-
-    // Start from the last element
-    int maxFromRight = nums[N - 1];
-    leaders[leaderCount++] = maxFromRight; // The last element is always a leader
-
-    // Traverse the array from right to left
-    for (int i = N - 2; i >= 0; i--) {
-        if (nums[i] >= maxFromRight) {
-            maxFromRight = nums[i]; // Update the maximum from the right
-            leaders[leaderCount++] = maxFromRight; // Store the leader
+    for(int i=0; i<N; i++)
+    {
+        int max=arr[i];
+        for(int j=i+1; j<N; j++)
+        {
+            if(arr[i]<arr[j]) //i.e. vo leader element nhi hai
+            {
+                break; //come out of inner forloop and check for next no. in i               
+            }
+            //agr vohi leader hai 
+            printf("%d ", max);
         }
-    }
 
-    // Print leaders in the order they appear in the original array
-    for (int i = leaderCount - 1; i >= 0; i--) {
-        printf("%d", leaders[i]);
-        if (i > 0) {
-            printf(" "); // Print space between numbers
-        }
     }
-    printf("\n"); // New line at the end
+    printf("%d ", arr[N-1]); //at last print last element
+    // // Array to store leaders
+    // int leaders[100]; // Assuming the maximum number of leaders is 100
+    // int leaderCount = 0;
+
+    // // Start from the last element
+    // int maxFromRight = nums[N - 1];
+    // leaders[leaderCount++] = maxFromRight; // The last element is always a leader
+
+    // // Traverse the array from right to left
+    // for (int i = N - 2; i >= 0; i--) {
+    //     if (nums[i] >= maxFromRight) {
+    //         maxFromRight = nums[i]; // Update the maximum from the right
+    //         leaders[leaderCount++] = maxFromRight; // Store the leader
+    //     }
+    // }
+
+    // // Print leaders in the order they appear in the original array
+    // for (int i = leaderCount - 1; i >= 0; i--) {
+    //     printf("%d", leaders[i]);
+    //     if (i > 0) {
+    //         printf(" "); // Print space between numbers
+    //     }
+    // }
+    // printf("\n"); // New line at the end
 
     return 0;
 }
