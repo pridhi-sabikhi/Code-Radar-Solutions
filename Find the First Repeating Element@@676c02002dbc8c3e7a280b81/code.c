868e98cea1e2f1b783e0d8ff95d737bb0d2ae786
pridhi-sabikhi <sabhikhimonika@gmail.com>
2025-03-31 22:10:27 +0530

    Solution to Find the First Repeating Element

--- Find the First Repeating Element@@676c02002dbc8c3e7a280b81/code.c
+++ Find the First Repeating Element@@676c02002dbc8c3e7a280b81/code.c
@@ -5,38 +5,45 @@ int main()
     int n;
     scanf("%d", &n);
     int arr[n];
+
+    //to print unique no. in array 
+    // for(int i=0; i<n; i++)
+    // {
+    //     scanf("%d", &arr[i]);
+    // }
+    // for(int i=0; i<n; i++)
+    // {
+    //     int c=0;
+    //     if(arr[i]!=INT_MIN)
+    //     {
+    //     for(int j=i+1; j<n;j++)
+    //     {
+    //         if(arr[i]==arr[j])
+    //         {
+    //             c++;
+    //             arr[j]=INT_MIN;
+    //         }
+    //     }
+    //     if(c==0)//w/m unique element 
+    //     printf("%d \n", arr[i]);  //Written inside of if not equal bcz we don't want to print INT_MIN
+    //     }
+    // }
+
     for(int i=0; i<n; i++)
     {
         scanf("%d", &arr[i]);
     }
-    for(int i=0; i<n; i++)
     {
-        int c=0;
-        if(arr[i]!=INT_MIN)
-        {
-        for(int j=i+1; j<n;j++)
+        for(int j=i+1; j<n; j++)//start j with index i+1 bcz pichle toh check ho chuke hai 
+        //and jis no. ko check krna ahi ussi no. ko dubara lekr usse hi repeated na smj le
         {
             if(arr[i]==arr[j])
             {
-                c++;
-                arr[j]=INT_MIN;
+               printf("%d", arr[i]);
+               return 0;
             }
         }
-        if(c==0)//w/m unique element 
-        printf("%d \n", arr[i]);  //Written inside of if not equal bcz we don't want to print INT_MIN
-        }
     }
-    // {
-    //     for(int j=i+1; j<n; j++)//start j with index i+1 bcz pichle toh check ho chuke hai 
-    //     //and jis no. ko check krna ahi ussi no. ko dubara lekr usse hi repeated na smj le
-    //     {
-    //         if(arr[i]==arr[j])
-    //         {
-    //            printf("%d", arr[i]);
-    //            return 0;
-    //         }
-    //     }
-    // }
-    //     printf("-1"); //no repeating element
+        printf("-1"); //no repeating element
     return 0;
 }
