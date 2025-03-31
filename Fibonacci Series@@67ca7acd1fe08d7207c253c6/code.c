6dfff1aefad26d507476310f9c7a53034a5c1989
pridhi-sabikhi <sabhikhimonika@gmail.com>
2025-04-01 00:37:34 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,24 +1,52 @@
-int fibo(int num)
+#include <stdio.h>
+// int fibo(int num)
+// {
+//     if(num==0)
+//     {
+//     return 0;
+//     }
+//     else if(num==1)
+//     {
+//     return 1;
+//     }
+//     else
+//     {
+//     return fibo(num-1)+fibo(num-2);
+//     }
+// }
+// int fibonacciSeries(int n)
+// {
+//     for(int i=0; i<n; i++)
+//     {
+//         int ans=fibo(i);
+//         printf("%d ", ans);
+//     }
+//     return 0;
+// }
+//or
+fibonacciSeries(int n)
 {
-    if(num==0)
+    int arr[n];
+    arr[0]=0;
+    arr[1]=1;
+    if(n==1)
     {
-    return 0;
+        printf("%d", arr[0]);
+        return 0;
     }
-    else if(num==1)
+    else if(n==2)
     {
-    return 1;
+        printf("%d %d", arr[0], arr[1]);
+        return 0;
     }
     else
     {
-    return fibo(num-1)+fibo(num-2);
-    }
-}
-int fibonacciSeries(int n)
-{
-    for(int i=0; i<n; i++)
+        printf("%d %d ", arr[0], arr[1]);
+    
+    for(int i=2; i<n; i++)
     {
-        int ans=fibo(i);
-        printf("%d ", ans);
+        arr[i]=arr[i-1]+arr[i-2];
+        printf("%d ", arr[i]);
     }
-    return 0;
+    }
 }
