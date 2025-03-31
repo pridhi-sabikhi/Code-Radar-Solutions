6fcfc58348163a3fb6d96548147687d80b731b9c
pridhi-sabikhi <sabhikhimonika@gmail.com>
2025-03-31 19:14:31 +0530

    Solution to Late Submission Penalty – Coding Contest

--- Late Submission Penalty – Coding Contest@@67de00956ac3fec9afb90e16/code.c
+++ Late Submission Penalty – Coding Contest@@67de00956ac3fec9afb90e16/code.c
@@ -1,23 +1,49 @@
 #include <stdio.h>
 
-int calculatePenalty(int subDay, int subMonth, int subYear, int dueDay, int dueMonth, int dueYear)
- {
-
-    if (subDay > dueDay && dueMonth == subMonth && subYear == dueYear) 
-    {
-        return ((subDay-dueDay)*10);
+int calculatePenalty(int subDay, int subMonth, int subYear, int dueDay, int dueMonth, int dueYear) {
+    // If the submission year is greater than the due year
+    if (subYear > dueYear) {
+        return 5000; // Fixed penalty for different year
     }
-    else if ((subMonth > dueMonth && subYear == dueYear)||(subDay==dueDay) ){
-            return ((subMonth - dueMonth) * 200); 
+    // If the submission year is the same as the due year
+    else if (subYear == dueYear) {
+        // If the submission month is greater than the due month
+        if (subMonth > dueMonth) {
+            return (subMonth - dueMonth) * 200; // Monthly penalty
         }
+        // If the submission month is the same as the due month
+        else if (subMonth == dueMonth) {
+            // If the submission day is greater than the due day
+            if (subDay > dueDay) {
+                return (subDay - dueDay) * 10; // Daily penalty
+            }
+        }
+    }
+    // If the submission is on or before the due date
+    return 0; // No penalty
+}
+
+//or 
+// #include <stdio.h>
+
+// int calculatePenalty(int subDay, int subMonth, int subYear, int dueDay, int dueMonth, int dueYear)
+//  {
+
+//     if (subDay > dueDay && dueMonth == subMonth && subYear == dueYear) 
+//     {
+//         return ((subDay-dueDay)*10);
+//     }
+//     else if ((subMonth > dueMonth && subYear == dueYear)||(subDay==dueDay) ){
+//             return ((subMonth - dueMonth) * 200); 
+//         }
         
-    else if (subYear > dueYear) //baaki conditions se no frq hame 
-    {
-        return 5000;
-    }
-    else //time pr eturn ki 
-    {
-        return 0;
-    }
+//     else if (subYear > dueYear) //baaki conditions se no frq hame 
+//     {
+//         return 5000;
+//     }
+//     else //time pr eturn ki 
+//     {
+//         return 0;
+//     }
       
-    }
+//     }
